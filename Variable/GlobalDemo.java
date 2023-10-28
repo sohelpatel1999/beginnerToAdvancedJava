4b5cf403ea1c4cbf97689c83ddb1db1bbdb68f76
sohelpatel1999 <patelsohel1999@gmail.com>
2023-10-28 13:42:21 +0530

    [Sohel] Added: initializing Example.

--- Variable/GlobalDemo.java
+++ Variable/GlobalDemo.java
@@ -8,8 +8,10 @@ public class GlobalDemo {
 
     public static void main(String[] args) {
         GlobalDemo globaldemo = new GlobalDemo();
-        System.out.println("This is the value of x " + globaldemo.x);
-        System.out.println("This is the value of y " + globaldemo.y);
+        System.out.println("This is the value of x " + globaldemo.x); //0
+        System.out.println("This is the value of y " + globaldemo.y); //110
+        globaldemo.x = globaldemo.y - 10; // initializing value to x
+        System.out.println("This is the value of x " + globaldemo.x); // 100
 
     }
 
