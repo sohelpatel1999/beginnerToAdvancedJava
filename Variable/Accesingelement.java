f83213596f3b989d77876f8c17797551bc8dd036
sohelpatel1999 <patelsohel1999@gmail.com>
2023-10-28 13:20:46 +0530

    [Sohel] Added: Accesing element from same Package.

---
+++ Variable/Accesingelement.java
@@ -0,0 +1,11 @@
+package Variable;
+
+public class Accesingelement {
+    public static void main(String[] args) {
+        // Accesing the element from same Package
+        GlobalDemo gb = new GlobalDemo();
+        System.out.println(gb.x);
+        System.out.println(gb.y);
+
+    }
+}
