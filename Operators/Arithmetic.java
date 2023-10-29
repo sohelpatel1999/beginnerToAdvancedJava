57a88d60f324a4de5f248b66f1a5711198b482a2
sohelpatel1999 <patelsohel1999@gmail.com>
2023-10-29 13:14:39 +0530

    [Sohel] Added: Arithmetic operator.

---
+++ Operators/Arithmetic.java
@@ -0,0 +1,23 @@
+package Operators;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        String c = "sohel";
+        
+
+        System.out.println("The sum of two digit is " + (a + b)); // 30
+
+        System.out.println("The minus of two digit is " + (a - b)); // 10
+
+        System.out.println("The Multiplication of two digit is " + (a * b)); // 200
+
+        System.out.println("The division of two digit is " + (a / b)); // 2
+
+        System.out.println("The modulos of two digit is " + (a % b)); // 0
+
+    }
+
+}
