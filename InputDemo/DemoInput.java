8b5c689db32e346bc05187e97e2cddb4534027a4
sohelpatel1999 <patelsohel1999@gmail.com>
2023-11-18 14:05:07 +0530

    [Sohel] Added: Input Demo Taking.

---
+++ InputDemo/DemoInput.java
@@ -0,0 +1,11 @@
+package InputDemo;
+
+import java.util.Scanner;
+
+public class DemoInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        // System.out.println(number);
+    }
+}
