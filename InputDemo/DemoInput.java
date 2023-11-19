1e1c0c7a7e9223005a73fe7a89d3cfcb39d446cf
sohelpatel1999 <patelsohel1999@gmail.com>
2023-11-19 18:18:18 +0530

    [Sohel] Added: taking Input from user.

--- InputDemo/DemoInput.java
+++ InputDemo/DemoInput.java
@@ -6,6 +6,7 @@ public class DemoInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
+        // 
         System.out.println(number);
     }
 }
