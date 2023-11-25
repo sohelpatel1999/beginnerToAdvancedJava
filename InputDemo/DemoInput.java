e504ce962d9c420f6de1a7dabfc655e045a92c9b
sohelpatel1999 <patelsohel1999@gmail.com>
2023-11-25 13:46:05 +0530

    [Sohel] Added :  Taking Input From User.

--- InputDemo/DemoInput.java
+++ InputDemo/DemoInput.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class DemoInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int number = sc.nextInt();
-        System.out.println(number);
+
+        String name = sc.next(); //sohel is good boy
+        sc.nextLine(); 
+        String name1 = sc.nextLine(); 
+
+        System.out.println("using only next " + number);
+        System.out.println("using only next " + name);
+        
+
+        System.out.println("number we enter is " + name1);
     }
 }
