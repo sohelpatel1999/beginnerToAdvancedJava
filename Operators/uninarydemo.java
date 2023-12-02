3c78b2027c7ecffe714d19526e124f68efd9357b
sohelpatel1999 <patelsohel1999@gmail.com>
2023-12-02 13:10:43 +0530

    [Sohel] Added: Uninary Operator Example.

---
+++ Operators/uninarydemo.java
@@ -0,0 +1,22 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class uninarydemo {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt(); //24
+        System.out.println(age++);   //24 increment by 1 = 25
+        System.out.println(age);     // 25
+
+        System.out.println(age--);
+        System.out.println(age);
+
+        System.out.println(++age); // 25
+        System.out.println(age);   //25
+
+        System.out.println(--age); //24
+        System.out.println(age);  //24
+    }
+}
