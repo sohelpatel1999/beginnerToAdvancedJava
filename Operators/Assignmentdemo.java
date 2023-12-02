4eac5512a75d699cbe4888b325ba14afdb6bc1af
sohelpatel1999 <patelsohel1999@gmail.com>
2023-12-02 13:09:24 +0530

    [Sohel] Added: Addignment Operator Example.

---
+++ Operators/Assignmentdemo.java
@@ -0,0 +1,24 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class Assignmentdemo {
+
+    public static void main(String[] args) {
+
+        // creating object of scanner class
+        // by using object call the specific method related to that datatype
+
+        Scanner neel = new Scanner(System.in);
+
+        int age = neel.nextInt();
+        System.out.println(" the age of the user is " + age);
+
+        int a = 20;
+        String name = "sohel isfiureiu ioherfijrfuh iuoherivbriuh ";
+        System.out.println("The value of a is >>>>>" + a);
+        System.out.println("The value of name is >>>>>" + name);
+
+    }
+
+}
