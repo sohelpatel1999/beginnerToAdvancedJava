6f8709470ef0786051b0fd70672b362165b92690
sohelpatel1999 <patelsohel1999@gmail.com>
2023-12-10 13:19:38 +0530

    [Sohel] Added: Shift Operator Example.

---
+++ Operators/Shiftdemo.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class Shiftdemo {
+
+    public static void main(String[] args) {
+        int a = 7;
+        System.out.println("Left Shift");
+        System.out.println(a << 4); // 0111    01110000
+        // 0001 << 0010
+
+
+        System.out.println("Right Shift");
+        System.out.println(a >> 3); //0111       0000 
+
+    }
+
+}
