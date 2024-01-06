7169e836d2cb353d06aaafaf464ab8d07d8af458
sohelpatel1999 <patelsohel1999@gmail.com>
2024-01-06 14:53:44 +0530

    [Sohel] Added: Else If Ladder Example.

---
+++ Controlflow/Elseifladder.java
@@ -0,0 +1,27 @@
+package Controlflow;
+
+import java.util.Scanner;
+
+public class Elseifladder {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the marks to check grades : ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90 && marks < 100) {
+            System.out.println("A");
+        } else if (marks >= 60 && marks < 90) {
+            System.out.println("B");
+        } else if (marks >= 50 && marks < 60) {
+            System.out.println("C");
+        } else if (marks >= 40 && marks < 50) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
+    }
+
+}
