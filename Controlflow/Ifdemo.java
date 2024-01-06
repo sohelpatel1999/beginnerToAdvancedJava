1202efcda3fd35f1f3cae8f8e10888c754e86fbe
sohelpatel1999 <patelsohel1999@gmail.com>
2024-01-06 14:52:35 +0530

    [Sohel] Added: If Example.

---
+++ Controlflow/Ifdemo.java
@@ -0,0 +1,21 @@
+package Controlflow;
+
+public class Ifdemo {
+    
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 20;
+
+        int answer = a+b;
+        System.out.println(answer);
+
+
+        if(a+b == 20){
+            System.out.println("if block excuted");
+        }
+
+
+
+    }
+}
