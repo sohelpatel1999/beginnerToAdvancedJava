7d90a648868cc51801b348d7b7cdbcf3aaefd3a9
sohelpatel1999 <patelsohel1999@gmail.com>
2024-01-15 11:17:24 +0530

    [Sohel] Added : Even Odd Logic.

--- Controlflow/Ifelsedemo.java
+++ Controlflow/Ifelsedemo.java
@@ -3,19 +3,23 @@ package Controlflow;
 public class Ifelsedemo {
     public static void main(String[] args) {
 
-        int num = 11;
+        int num = 99;
 
-        if (num % 2 == 0) {
-            System.out.println(" number is even");
-        } else {
+        int ans = num % 2;  //98 + 1 9=  8+ 1
+        System.out.println("Answer is " + ans);
+
+
+        if (ans == 1) {
             System.out.println(" number is odd");
+        } else {
+            System.out.println(" number is even");
         }
 
         // int a = 10;
         // int b = 20;
 
-        String answer = (num % 2 == 0) ? "number is even" : "number is odd";
-        System.out.println(answer);
+        // String answer = (num % 2 == 0) ? "number is even" : "number is odd";
+        // System.out.println(answer);
 
     }
 }
