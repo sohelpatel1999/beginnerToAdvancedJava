1736bff0eeabdfd2dc51350fbd1ca0614503b2f2
sohelpatel1999 <patelsohel1999@gmail.com>
2024-01-15 11:18:12 +0530

    [Sohel] Added: Percentage Example.

--- Operators/Arithmetic.java
+++ Operators/Arithmetic.java
@@ -3,20 +3,42 @@ package Operators;
 public class Arithmetic {
 
     public static void main(String[] args) {
-        int a = 20;
-        int b = 10;
-        String c = "sohel";
+        // int a = 20;
+        // int b = 10;
+        // String c = "sohel";
         
 
-        System.out.println("The sum of two digit is " + (a + b)); // 30
+        // System.out.println("The sum of two digit is " + (a + b)); // 30
 
-        System.out.println("The minus of two digit is " + (a - b)); // 10
+        // System.out.println("The minus of two digit is " + (a - b)); // 10
 
-        System.out.println("The Multiplication of two digit is " + (a * b)); // 200
+        // System.out.println("The Multiplication of two digit is " + (a * b)); // 200
 
-        System.out.println("The division of two digit is " + (a / b)); // 2
+        // System.out.println("The division of two digit is " + (a / b)); // 2
 
-        System.out.println("The modulos of two digit is " + (a % b)); // 0
+        // System.out.println("The modulos of two digit is " + (a % b)); // 0
+
+        int amount = 100;
+        //case 1
+        int percentage = (10/100) ; // 0.1  0
+        //case 2 
+        double per = (10/100) ; // 0.1  0
+        double test = 0;
+
+        //case 3
+        int sohel = (int)((10.0/100) * 100);
+
+        // case 4
+        // int val = (10.0/100);
+
+
+        System.out.println(" the Percentage is  " + percentage);
+        System.out.println(" the Percentage is  " + per);
+        System.out.println(" the Percentage is  " + test);
+        System.out.println(" the Percentage is  " + sohel);
+
+
+
 
     }
 
