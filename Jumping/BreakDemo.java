1f025cd51ca1822a9bad1b99b8fdcdc75d99b0c8
sohelpatel1999 <patelsohel1999@gmail.com>
2024-02-10 13:50:52 +0530

    [Sohel] Jumping Break Demo.

---
+++ Jumping/BreakDemo.java
@@ -0,0 +1,12 @@
+package Jumping;
+
+public class BreakDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i == 10) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
