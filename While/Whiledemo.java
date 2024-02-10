84c679e3e2c0822c448ed44cb3cfd68679c36c75
sohelpatel1999 <patelsohel1999@gmail.com>
2024-02-10 13:50:16 +0530

    [Sohel] While Loop Demo.

---
+++ While/Whiledemo.java
@@ -0,0 +1,25 @@
+package While;
+
+public class Whiledemo {
+    public static void main(String[] args){
+        //Step 1 initilization
+        int i = 0;
+
+        //Step while block
+        while(i<6){
+            System.out.println(i);
+            i++; //increment 
+        }
+        System.out.println("end of first loop");
+
+        // int j = 5;
+
+        //Step while block
+        while(i>0){
+            System.out.println(i);
+            i--; //decrement 
+        }
+
+
+    }
+}
