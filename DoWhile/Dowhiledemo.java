ede1cf788806963c56bd488275f0a4f9315263f3
sohelpatel1999 <patelsohel1999@gmail.com>
2024-02-10 13:50:34 +0530

    [Sohel] Do While Loop Demo.

---
+++ DoWhile/Dowhiledemo.java
@@ -0,0 +1,17 @@
+package DoWhile;
+
+public class Dowhiledemo {
+    public static void main(String[] args) {
+
+        //Step 1 initilization
+        int i = 0;
+
+        //Step 2 
+        do{
+            System.out.println("this is do block");
+            System.out.println(i);
+            i++;
+        } while (i>5);
+        
+    }
+}
