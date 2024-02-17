e6f2ef69781a5923bf4b0864ea72501646a2bc0f
sohelpatel1999 <patelsohel1999@gmail.com>
2024-02-17 14:00:43 +0530

    [Sohel] Added: Prinitng number.

---
+++ Forloopdemo/Printnumber.java
@@ -0,0 +1,14 @@
+package Forloopdemo;
+
+public class Printnumber {
+
+    public static void main(String[] args) {
+        
+        //1 2 3 4 5 
+        for(int i = 1;i<6 ;i++){
+            System.out.print(i + " ");
+        }
+
+    }
+    
+}
