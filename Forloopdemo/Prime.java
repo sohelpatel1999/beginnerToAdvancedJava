a9a208e4afe4ed39bdff4d959b8ba3c76b596841
sohelpatel1999 <patelsohel1999@gmail.com>
2024-02-17 14:01:15 +0530

    [Sohel] Added: Prime Number Logic.

---
+++ Forloopdemo/Prime.java
@@ -0,0 +1,24 @@
+package Forloopdemo;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        int count = 0;
+        //7 , 24 , 1
+        for (int i = 1; i <= num; i++) {
+            if(num%i==0){
+                count++;  //3 12
+            }
+        }
+        if (count==2) {
+            System.out.println("number is Prime");
+        } else{
+            System.out.println("number is not prime");
+        }
+
+    }
+}
