2f470353bfb90f37947f2639efa6f7cf87243d59
sohelpatel1999 <patelsohel1999@gmail.com>
2024-02-24 14:32:06 +0530

    [Sohel] Created : Prime Number Question 1 to 20.

---
+++ Foundation/Problem1.java
@@ -0,0 +1,19 @@
+public class Problem1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            // System.out.println(i);
+
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2 && (i%2!=0)) {
+                System.out.println("ye number prime hai kyuki ye sirf do table mai hi divisible hore hai " + i);
+            }
+
+        }
+    }
+}
