1aa45912ea0338dc85a4c4dca672a43f207f835e
sohelpatel1999 <patelsohel1999@gmail.com>
2024-03-09 14:41:12 +0530

    Changes the Name.

---
+++ Operators/Relationaldemo.java
@@ -0,0 +1,32 @@
+package Operators;
+
+public class Relationaldemo {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 10;
+
+        // Strictly Greater than
+        System.out.println(a < b); // 10<20 = true.
+
+        // Strictly lesser than
+        System.out.println(a > b); // 10>20 = false.
+
+        // greater than equal to
+        System.out.println(a <= b); // 10 <=20 = true
+
+        // lesser than equal to
+        System.out.println(a >= b); // 10 >=20 = false
+
+        // Strictly Equal to
+        System.out.println(a == b); // 10 == 20 = false.
+
+        // Not Equal to
+        System.out.println(a != b); // 10 != 20 = true.
+
+        System.out.println(a != c); // 10 != 10 = false.
+
+    }
+
+}
