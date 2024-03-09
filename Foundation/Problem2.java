37a64df8eb8c69939827f82e1c5d99f99c92c7af
sohelpatel1999 <patelsohel1999@gmail.com>
2024-03-09 14:43:41 +0530

    basic of method.

---
+++ Foundation/Problem2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       Problem2 pb2 = new Problem2();
+       int num = 5;
+       int factanswer = fact(num);
+       System.out.println(factanswer);
+    }
+
+    public static int fact(int neel){
+        System.out.println(neel + "  got from calling fact method on 7th line");
+        return 0;
+    }
+
+
+    
+
+}
