9ab22ca9266684c32026bac204776e4f584cb470
sohelpatel1999 <patelsohel1999@gmail.com>
2024-05-12 14:36:48 +0530

    Constructor 1.no Argument, 2. paratermized , 3.default.

--- constructor/ConstructorDemo.java
+++ constructor/ConstructorDemo.java
@@ -1,21 +1,41 @@
 package constructor;
 
 public class ConstructorDemo {
+    
 
     String name;
     int age;
 
-    ConstructorDemo(){
-        name = " sohe patel";
-        age = 14;
+    // no argument
+    ConstructorDemo() {
+        super();
+        name = "Human";
+        age = 100;
+    }
+
+    // paretermized constructor
+    ConstructorDemo(String givenname) {
+        name = givenname;
+    }
+
+    // paretermized constructor
+    ConstructorDemo(String givenname, int givenage) {
+        name = givenname;
+        age = givenage;
     }
 
     public static void main(String[] args) {
-        ConstructorDemo sohel = new ConstructorDemo();
-        System.out.println("this is sohel object");
-        System.out.println(sohel.name + " this is name");
-        System.out.println(sohel.age + " this is age");
+        // ConstructorDemo sohel = new ConstructorDemo("sohel",14);
+        // ConstructorDemo sohel = new ConstructorDemo("sohel",24);
+        // System.out.println("this is sohel object");
+        // // sohel.name = "sohel";
+        // // sohel.age = 24;
+        // System.out.println(sohel.name + " this is name");
+        // System.out.println(sohel.age + " this is age");
 
+        ConstructorDemo neel = new ConstructorDemo();
+        System.out.println(neel.name + " this is name");
+        System.out.println(neel.age + " this is age");
     }
 
 }
